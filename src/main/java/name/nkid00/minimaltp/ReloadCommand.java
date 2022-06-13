09610234edb0087895ec108c5c7826d2574cc7a2
NKID00 <nkid00@pm.me>
2022-06-14 00:21:10 +0800

    fix: incorrect reload command

--- src/main/java/name/nkid00/minimaltp/ReloadCommand.java
+++ src/main/java/name/nkid00/minimaltp/ReloadCommand.java
@@ -17,7 +17,7 @@ public class ReloadCommand {
     public static void register(CommandDispatcher<ServerCommandSource> dispatcher, CommandRegistryAccess registryAccess,
             CommandManager.RegistrationEnvironment environment) {
         dispatcher.register(
-                literal("/reload").requires(source -> source.hasPermissionLevel(2)).executes(TpCommand::execute));
+                literal("/reload").requires(source -> source.hasPermissionLevel(2)).executes(ReloadCommand::execute));
     }
 
     public static int execute(CommandContext<ServerCommandSource> c) throws CommandSyntaxException {
